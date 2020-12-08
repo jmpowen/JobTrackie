1e6530ecf9c8d7ea72d3fbad604ebd7c9f6e76ff
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2020-12-08 06:48:23 +0530

    Update formatting, remove yarn.lock, and update .gitignore

--- website/src/containers/App/index.js
+++ website/src/containers/App/index.js
@@ -1,14 +1,14 @@
-import Home from "../Home";
-import Login from "../Login";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from '../Home';
+import Login from '../Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
         </Switch>
       </Router>
     </div>
