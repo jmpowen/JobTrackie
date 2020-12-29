9e9118ef64feb842850e23df9e6a562cd1795918
unknown <powen.jack@gmail.com>
2020-12-29 16:28:03 -0600

    - built out sign up page  - used material-ui for all pages  - can probably refactor at some point and make components out of some parts of this component, this will be done later if at all

--- website/src/containers/SignUp/index.js
+++ website/src/containers/SignUp/index.js
@@ -4,6 +4,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import InputLabel from '@material-ui/core/InputLabel';
 import Button from '@material-ui/core/Button';
 import { blue } from '@material-ui/core/colors';
@@ -25,34 +26,43 @@ const useStyles = makeStyles({
   }
 })
 
-export default function Login() {
+export default function SignUp() {
   const classes = useStyles();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [verifyPW, setVerifyPW] = useState('');
 
-  const handleLoginClick = event => {
+  const handleSignUpClick = event => {
     event.preventDefault()
+    
   }
 
   return (
     <Card className={classes.root} variant='outlined'>
-      <CardHeader className={classes.header} title='Login'/>
+      <CardHeader className={classes.header} title='Sign Up'/>
       <CardActions>
         <FormControl className={classes.form}>
           <InputLabel htmlFor='my-input'>Email address</InputLabel>
-          <Input value={email} onChange={e => setEmail(e.target.value)} id='email' aria-describedby='my-helper-text' />
+          <Input value={email} onChange={e => setEmail(e.target.value)} id='email' />
+          <FormHelperText id='my-helper-text'>We'll never share your email.</FormHelperText>{/** This should go on Signup page */}
         </FormControl>
       </CardActions>
       <CardActions>
         <FormControl className={classes.form}>
           <InputLabel htmlFor='my-input'>Password</InputLabel>
-          <Input value={password} onChange={e => setPassword(e.target.value)} type='password' id='password' aria-describedby='my-helper-text' />
+          <Input value={password} onChange={e => setPassword(e.target.value)} type='password' id='password' />
         </FormControl>
       </CardActions>
       <CardActions>
-        <Button onClick={handleLoginClick} className={classes.form}>
-          Login
+        <FormControl className={classes.form}>
+          <InputLabel htmlFor='my-input'>Verify Password</InputLabel>
+          <Input value={verifyPW} onChange={e => setVerifyPW(e.target.value)} type='password' id='verify-password' />
+        </FormControl>
+      </CardActions>
+      <CardActions>
+        <Button onClick={handleSignUpClick} className={classes.form}>
+          Sign Up
         </Button>
       </CardActions>
     </Card>
