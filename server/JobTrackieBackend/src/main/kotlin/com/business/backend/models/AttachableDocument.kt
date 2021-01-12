29bd41f89d544155b59bbc2832aeca3c7589e5c4
wasartin <wasartin@iastate.edu>
2021-01-12 12:05:02 -0600

    Added dependencies for model classes

--- server/JobTrackieBackend/src/main/kotlin/com/business/backend/models/AttachableDocument.kt
+++ server/JobTrackieBackend/src/main/kotlin/com/business/backend/models/AttachableDocument.kt
@@ -1,7 +1,15 @@
 package com.business.backend.entities
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Document(collection = "attachableDocument")
 data class AttachableDocument(
         var name: String,
