209ba5cb9105fe3072f3fd43191a57e39f385fc9
wasartin <wasartin@iastate.edu>
2021-02-02 22:05:50 -0600

    Created app skeleton, setup .gitignore, and added necessary dependencies.

---
+++ server/JobApplications/src/test/kotlin/com/jobtrackie/jobapplications/JobApplicationsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jobtrackie.jobapplications
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JobApplicationsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
